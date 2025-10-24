d06ba03f8abc3e07b6c1396221744f7f2c488565
Pustulkapiotr20251094 <123piotr1234@gmail.com>
2025-10-24 10:42:34 +0200

    Dodano metody pushFront, pushBack i displayForward

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include "lista.h"
+
+
 int main()
 {
+	lista lista;
+
 	std::cout << "Projekt - lista_dwukierunkowa"<<'\n';
 	std::cout << "\n";
-	std::cout << "test - tworzenie obiektu \n";
-	lista l;
-	std::cout << "rozmiar poczatkowy : " << l.size() << "\n";
-	std::cout << (l.empy() ? "lista jest pusta " : "coœ tam jest ");
+	
+	std::cout << "dodawanie leementu na koniec listy \n";
+	lista.pushBack(4);
+	lista.pushBack(6);
+	lista.displayForward();
+
+
+
+
+
+	std::cout << "\n Dodawnia elementu na pocz¹tek listy  \n";
+	lista.pushFront(1);
+	lista.pushFront(2);
+	lista.pushFront(3);
+	lista.displayForward();
+
+
+	std::cout << "\nRozmiar listy: " << lista.size() << "\n";
+
 
 
 
